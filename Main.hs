8cc2f5500629535d9c277cebe5772d182a0f03fc
Josh Hickman <IRespondPromptlyWhenContacted@gmail.com>
2014-11-24 17:45:33 -0500

    adding subdomain support

--- Main.hs
+++ Main.hs
@@ -1,11 +1,24 @@
 module Main where
 
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok, ServerPart)
+import Control.Monad (guard, msum, void)
+import Control.Monad.Trans  (liftIO)
+import Data.List.Split (splitOn)
+import Happstack.Server (nullConf, simpleHTTP, toResponse, ok, ServerPart, withHost)
 
 main :: IO ()
 main = do
      putStrLn "Starting Server..."
-     simpleHTTP nullConf $ site
-     
+     simpleHTTP nullConf site
+
 site :: ServerPart String
-site = ok "Sup, World!"
+site = msum [subsite "fish" $ ok "Hello, Fish!",
+             subsite "box" $ ok "Hello, Box!",
+             ok "Sup, World!"]
+
+subsite :: String -> ServerPart a -> ServerPart a
+subsite subdomain handler = withHost conditionally
+  where conditionally host = let parts = splitOn "." host
+                                 visitableDomain = length parts == 3
+                                 thisDomain = head parts == subdomain
+                                 shouldServe = visitableDomain && thisDomain
+                             in guard shouldServe >> handler
