39aaeb8bec414a6beafa6e326d18c6482d6167b8
Josh Hickman <IRespondPromptlyWhenContacted@gmail.com>
2014-11-25 00:25:50 -0500

    throw the beat timer app in here

--- Main.hs
+++ Main.hs
@@ -1,19 +1,24 @@
 module Main where
 
 import Control.Monad (guard, msum, void)
-import Control.Monad.Trans  (liftIO)
+import Control.Monad.Trans (liftIO)
 import Data.List.Split (splitOn)
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok, ServerPart, withHost)
+import Happstack.Server (nullConf, simpleHTTP, toResponse,
+                         ok, ServerPart, withHost, serveDirectory,
+                         Browsing(DisableBrowsing), Response)
 
 main :: IO ()
 main = do
      putStrLn "Starting Server..."
      simpleHTTP nullConf site
 
-site :: ServerPart String
-site = msum [subsite "fish" $ ok "Hello, Fish!",
-             subsite "box" $ ok "Hello, Box!",
-             ok "Sup, World!"]
+site :: ServerPart Response
+site = msum [subsite "timer" timer,
+             subsite "box" $ ok $ toResponse "Hello, Box!",
+             ok $ toResponse "Sup, World!"]
+
+timer :: ServerPart Response
+timer = serveDirectory DisableBrowsing ["index.html"] "./pulse/www"
 
 subsite :: String -> ServerPart a -> ServerPart a
 subsite subdomain handler = withHost conditionally
